f332e7d8ac1cf9546445becff93fbb2dd7a5f772
lehahoang <37591808+lehahoang@users.noreply.github.com>
2020-03-11 18:16:17 +0100

    Add files via upload

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -2,9 +2,9 @@
 import torch
 from torchvision import datasets, transforms
 from torch import utils
+import numpy as np
 
-
-def mnist_loader(train_batch_size, test_batch_size):
+def mnist_loader(args, train_batch_size, test_batch_size):
     kwargs = {'num_workers': 1, 'pin_memory': True}
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.1307,), (0.3081,))])
@@ -16,19 +16,21 @@ def mnist_loader(train_batch_size, test_batch_size):
                              train=False,
                              download=True,
                              transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset=train_set,
+    train_loader = utils.data.DataLoader(dataset=train_set,
                                                batch_size=train_batch_size,
                                                shuffle=True,
                                                **kwargs)
 
-    test_loader = torch.utils.data.DataLoader(dataset=test_set,
+    # val_loader =
+    test_loader = utils.data.DataLoader(dataset=test_set,
                                               batch_size=test_batch_size,
                                               shuffle=True,
                                               **kwargs)
 
     return train_loader, test_loader
+    # return train_loader, val_loader, test_loader
 
-def cifar10_loader(train_batch_size, test_batch_size):
+def cifar10_loader(args, train_batch_size, test_batch_size):
     kwargs = {'num_workers': 4, 'pin_memory': True}
     transform = transforms.Compose([transforms.ToTensor(),
                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -36,22 +38,41 @@ def cifar10_loader(train_batch_size, test_batch_size):
                                  train=True,
                                  download=True,
                                  transform=transform)
+    val_set = datasets.CIFAR10(root='./dataset',
+                                 train=True,
+                                 download=True,
+                                 transform=transform)
     test_set = datasets.CIFAR10(root='./dataset',
                                 train=False,
                                 download=True,
                                 transform=transform)
+    num_train = len(train_set)
+    indices = list(range(num_train))
+    split = int(np.floor(args.split_fraction * num_train))
+    print('xxxxxxxxxxxxxxxcheck', split)
+    np.random.seed(args.seed)
+    np.random.shuffle(indices)
+    train_sampler= utils.data.sampler.SubsetRandomSampler(indices[split:])
+    val_sampler = utils.data.sampler.SubsetRandomSampler(indices[:split])
 
     train_loader = utils.data.DataLoader(dataset=train_set,
                                          batch_size=train_batch_size,
-                                         shuffle=True,
+                                         sampler=train_sampler,
                                          **kwargs)
+    val_loader = utils.data.DataLoader(dataset=train_set,
+                                     batch_size=train_batch_size,
+                                     sampler=val_sampler,
+                                     **kwargs)
+    print(len(val_loader))
+    print(len(train_loader))
     test_loader = utils.data.DataLoader(dataset=test_set,
                                         batch_size=test_batch_size,
                                         shuffle=False,
                                         **kwargs)
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
 
-def cifar100_loader(train_batch_size, test_batch_size):
+
+def cifar100_loader(args, train_batch_size, test_batch_size):
     kwargs = {'num_workers': 4, 'pin_memory': True}
     train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
                                          transforms.RandomHorizontalFlip(),
@@ -78,3 +99,4 @@ def cifar100_loader(train_batch_size, test_batch_size):
                                               shuffle=False,
                                               **kwargs)
     return train_loader, test_loader
+# return train_loader, val_loader, test_loader
